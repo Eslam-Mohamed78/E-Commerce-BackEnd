0db7723f3acd83bbbacd944e5b4b7a9c25966245
Eslam-Mohamed78 <eslam31043206@f-eng.tanta.edu.eg>
2023-09-01 15:49:25 +0300

    "put logo in images folder"

--- src/utils/createInvoice.js
+++ src/utils/createInvoice.js
@@ -15,7 +15,7 @@ export default function createInvoice(invoice, path) {
 
 function generateHeader(doc) {
   doc
-    .image("logo.jpg", 50, 45, { width: 50 })
+    .image("./../../images/logo.jpg", 50, 45, { width: 50 })
     .fillColor("#444444")
     .fontSize(20)
     .text("ACME Inc.", 110, 57)
