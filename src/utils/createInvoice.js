15a3efe584f02b5d7292dbc9a82c0e2e86f4d107
Eslam-Mohamed78 <eslam31043206@f-eng.tanta.edu.eg>
2023-09-02 06:36:07 +0300

    fix logo issue

--- src/utils/createInvoice.js
+++ src/utils/createInvoice.js
@@ -15,7 +15,7 @@ export default function createInvoice(invoice, path) {
 
 function generateHeader(doc) {
   doc
-    .image("../../images/logo.jpg", 50, 45, { width: 50 })
+    .image("./../../logo.jpg", 50, 45, { width: 50 })
     .fillColor("#444444")
     .fontSize(20)
     .text("ACME Inc.", 110, 57)
